72a2dcbf839b0c63c5ab40344358135695f64b83
Balarubinan <sudhagouthirubi@gmail.com>
2020-05-04 18:46:35 -1200

    Create signup form and signup autherisation

--- source/flask_app.py
+++ source/flask_app.py
@@ -1,12 +1,23 @@
 from flask import Flask,render_template,flash,redirect,url_for,request
 from source import login_from
-from source.login_from import Form
+from source.login_from import Form,SignUpForm
 app = Flask(__name__)
 app.config['SECRET_KEY']='raj'
 
 @app.route('/Farmer_page')
 def farmer_page():
     return render_template('farmer_page.html')
+
+@app.route('/Retailer_page')
+def retialer_page():
+    return render_template('retailer_page.html')
+
+@app.route('/Auction_page')
+def auction_page():
+    print("Visited")
+    lis=["Auction1","Auction2","Auction3"] # use the data fetched from the db
+    # as the input list here
+    return render_template('auction_page.html',auction_list=lis)
 
 @app.route('/signup')
 def index():
@@ -25,15 +36,22 @@ def mainpage():
         return redirect(url_for(f'{form.acc_type.data.lower()}_page'))
     return render_template('main_page.html',form=form)
 
-@app.route('/login',methods=['GET','POST'])
-def log_on():
-    form=Form()
-    if form.validate_on_submit():
-        # here iss where i should use my good old sqldatabase skills to store the damn files
-        flash(f'Login requested for user {form.username.data} and {form.remember.data}')
-        return redirect('/')
-    return render_template('login_template.html',title='SIGN IN',form=form)
+# The below route  wont be required as he maain page itself serves as a login page
+# @app.route('/login',methods=['GET','POST'])
+# def log_on():
+#     form=Form()
+#     if form.validate_on_submit():
+#         flash(f'Login requested for user {form.username.data} and {form.remember.data}')
+#         return redirect('/')
+#     return render_template('login_template.html',title='SIGN IN',form=form)
 
+@app.route('/signup',methods=['GET','POST'])
+def signup():
+    form=SignUpForm()
+    if form.username!=None or form.username!='':
+        pass # insert db operations here
+        return redirect('/')
+    return render_template('signup.html',title='SIGN IN',form=form)
 
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1')
--- source/login_from.py
+++ source/login_from.py
@@ -1,10 +1,23 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,BooleanField,PasswordField,SubmitField,RadioField
 from wtforms.validators import DataRequired
+from wtforms.fields.html5 import EmailField
 
+# this class obejct is used to hold the sigin form for aldready exsting users
 class Form(FlaskForm):
     username=StringField('Username',validators=[DataRequired()])
     password=PasswordField('Password',validators=[DataRequired()])
     remember=BooleanField("Know me??")
-    acc_type=RadioField('Account type',validators=[DataRequired()],choices=['Farmer','Retailer'])
+    # acc_type=RadioField('Account type',validators=[DataRequired()],choices=['Farmer','Retailer'])
+    # we dont need this field as we can fetch the data from the
     submit=SubmitField('Sign in')
+
+# this class is used to hold the Signup form
+class SignUpForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password1 = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired()])
+    remember = BooleanField("Know me??")
+    email=EmailField('Email address',validators=[DataRequired()])
+    acc_type = RadioField('Account type', validators=[DataRequired()], choices=['Farmer', 'Retailer'])
+    submit = SubmitField('Sign up')
